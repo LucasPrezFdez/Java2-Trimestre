03bcff7d64ff2dd091ff0a3f03f5bf87f54e3d4b
Lukas <lucaslinoperez@gmail.com>
2025-01-15 13:36:36 +0100

    15/1/25

---
+++ src/main/java/arrays/AlgoritmoBurbuja.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class AlgoritmoBurbuja {
+
+    public static void main(String[] args) {
+        int[] array = {6, 5, 3, 1, 8, 7, 2, 4};
+        int n = array.length;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int aux = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = aux;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
